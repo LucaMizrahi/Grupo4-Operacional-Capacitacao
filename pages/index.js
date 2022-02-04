81fdbfbb7563d5d9dc3e88749e5ae9d590039936
Luca Mizrahi <lucam1@al.insper.edu.br>
2022-02-04 01:36:48 -0300

    Pág Home finalizada

--- pages/index.js
+++ pages/index.js
@@ -16,46 +16,63 @@ export default function Home() {
           <div className={styles.logochefe}>
           <Image src="/chef.png" alt="Chef Logo" width={120} height={120} />
           </div>
-          <div className={styles.container4}>
-            <div className={styles.figurabarra}>
-              <Image src="/telemarketing.png" alt="Suporte" width={70} height={70} />
-            </div>
+          <div className={styles.figurabarra1}>
+            <Image src="/telemarketing.png" alt="Suporte" width={70} height={70} />
           </div>
-          <div className={styles.container5}>
-            <div className={styles.quemsomos}>
+          <div className={styles.quemsomos}>
             Quem Somos
-            </div>
           </div>
-          <div className={styles.container6}>
-            <div className={styles.sejachefe}>
-              Seja chefe
-            </div>
+          <div className={styles.sejachefe}>
+            Seja chefe
           </div>
-          <div className={styles.container7}>
-            <div className={styles.figurabarra}>
-            <Image src="/perfil.png" alt="Perfil Logo" width={80} height={80} />
-            </div>
-          </div>
+          <div className={styles.figurabarra2}>
+            <Image src="/perfil.png" alt="Perfil Logo" width={70} height={70} />
+          </div> 
         </h1>
-        <div class={styles.container2}>
-          <p className={styles.description}>
-            Ache o chef que mais combina com você!
-          </p>
+        
+        <div className={styles.container2}>
+          <div className={styles.description}>
+          Ache o chef que mais combina com você!
+          </div>
         </div>
-        <div class={styles.container3}>
-          oi
-          <div class={styles.container2}>
+        <div className={styles.container3}>
+          <div className={styles.filtroendereço}>
+            <div className={styles.filtrotexto}>
+            Endereço
+            <p>Onde vai ser o jantar?</p>
+            </div>
+          </div>
+          <div className={styles.filtroculinaria}>
+            < div className={styles.filtrotexto}>
+            Culinária
+            <p>Qual o tipo de comida?</p>
+            </div>
+          </div>
+          <div className={styles.filtrodata}>
+            <div className={styles.filtrotexto}>
+            Data
+            <p>Quando?</p>
+            </div>
+          </div>
+          <div className={styles.filtroqnt}>
+            <div className={styles.filtrotexto}>
+            Quantidade
+            <p>Quantas pessoas?</p>
+            </div>
+          </div>
+          <div className={styles.filtrobotao}>
+            <Image src="/Lupinha.png" alt="Lupinha" width={50} height={50} />
+          </div>
         </div>
-          <div class={styles.wrapper}>
-            <div class={styles.box1}>
-              <Image src="/Foto 1.png" alt="Foto 1" width={1010} height={430} /> 
-            </div>
-            <div class={styles.box2}>
-                <Image src="/Foto 3.png" alt="Foto 3" width={1006} height={322} />
-            </div>
-            <div class={styles.box3}>
-              <Image src="/Foto 2.png" alt="Foto 2" width={500} height={754} />
-            </div>
+        <div class={styles.wrapper}>
+          <div class={styles.box1}>
+            <Image src="/Foto 1.png" alt="Foto 1" width={1010} height={430} /> 
+          </div>
+          <div class={styles.box2}>
+              <Image src="/Foto 3.png" alt="Foto 3" width={1006} height={322} />
+          </div>
+          <div class={styles.box3}>
+            <Image src="/Foto 2.png" alt="Foto 2" width={500} height={754} />
           </div>
         </div>
       </main>
